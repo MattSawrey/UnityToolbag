ef3c71460984583609f291bfeb7703c5707139fb
Matthew Sawrey <matski53@gmail.com>
2017-02-27 21:12:49 +0000

    Tested all included scripts.

---
+++ Statics/SaveLoad.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveLoad
+{
+    #region - Binary
+    public static void SaveAsBinary(object objectToSerialize, string fileName)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/" + fileName);
+        bf.Serialize(file, objectToSerialize);
+        file.Close();
+    }
+
+    public static T LoadFromBinary<T>(string fileName)
+    {
+        if (File.Exists(Application.persistentDataPath + "/" + fileName))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/" + fileName, FileMode.Open);
+            T result = (T)bf.Deserialize(file);
+            file.Close();
+            return result;
+        }
+        else
+            throw new Exception("Attempting to load data from: " + fileName + ". File could not be found");
+    }
+    #endregion
+}
