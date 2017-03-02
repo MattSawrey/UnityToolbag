71947e99ede3964a3580b7cecaa0cf85114a6be2
Matthew Sawrey <matski53@gmail.com>
2017-03-02 21:10:34 +0000

    Added Reusable Components folder and more float extensions

--- Extension Methods/FloatExtensions.cs
+++ Extension Methods/FloatExtensions.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 
 public static class FloatExtensions 
 {
-	public static float ConvertToRange(this float oldValue, float oldMin, float oldMax,
-	                                         float newMin, float newMax)
+	public static float ConvertToRange(this float oldValue, float oldMin, float oldMax, float newMin, float newMax)
 	{
         return (((oldValue - oldMin) * (newMax - newMin)) / (oldMax - oldMin) + newMin); 
-	}	
+	}
+
+    public static float RoundTo(this float value, int numDecimalPoints)
+    {
+        int roundingFactor = (int)Mathf.Pow(10, numDecimalPoints);
+        Debug.Log(roundingFactor);
+        return Mathf.Round(value * roundingFactor) / roundingFactor;
+    }
+
+    public static float PercentageOf(this float value, float pcOfValue)
+    {
+        return ((pcOfValue - (pcOfValue - value)) / pcOfValue) * 100f;
+    }
+
+    public static float PercentageOf(this float value, float pcOfValue, int numDecimalPoints)
+    {
+        return value.PercentageOf(pcOfValue).RoundTo(numDecimalPoints);
+    }
 }
---
+++ Reusable Components/SpeedLimiter2D.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Limits the speed at which any 2D physics object with a rigidbody can travel. This can help avoid issues with physics objects "teleporting" (traveling so fast that they move past other objects in a frame an fail to interact them). This isn't a hard limit, but does dampen speeds above the max speed.
+/// </summary>
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SpeedLimiter2D : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    [Range(0.1f, 10f)]
+    public float dampeningFactor = 5f;
+
+    private Rigidbody2D rigidBody;
+
+    /// <summary>
+    /// Method does not directly set the velocity of the object as doing so interferes with the physics engine. It instead applies a counter force to the object to balance the speed out to the maximum value.
+    /// </summary>
+    public void LimitMaxVelocity()
+    {
+        float currentSpeed = Vector2.SqrMagnitude(rigidBody.velocity);
+
+        if (currentSpeed > maxSpeed)
+        {
+            float speedDifference = (currentSpeed - maxSpeed)/currentSpeed;
+            Vector2 brakeForce = -rigidBody.velocity*speedDifference;
+            rigidBody.AddForce(brakeForce*dampeningFactor);
+
+
+            //float newSpeed = Vector2.SqrMagnitude(rigidBody.velocity);
+            //Debug.Log("Old Speed: " + currentSpeed + ", New Speed: " + newSpeed);
+            //Debug.Log("Old Velocity: " + rigidBody.velocity + ", Brake Force: " + brakeForce);
+            //Debug.Log(currentSpeed);
+        }
+    }
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        LimitMaxVelocity();
+    }
+}
