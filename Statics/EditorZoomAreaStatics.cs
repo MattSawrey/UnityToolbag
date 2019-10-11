058b163c447623cc924bc721b88afd0125e6c0da
Matthew Sawrey <matski53@gmail.com>
2019-10-11 08:51:53 +0100

    Added RectExtensions and EditorGUIStatics.

---
+++ Statics/EditorZoomAreaStatics.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/// <summary>
+/// A simple class providing static access to functions that will provide a 
+/// zoomable area similar to Unity's built in BeginVertical and BeginArea
+/// Systems. Code based off of article found at:
+/// http://martinecker.com/martincodes/unity-editor-window-zooming/
+///  
+/// (Site may be down)
+/// </summary>
+public class EditorZoomAreaStatics
+{
+    private static Stack<Matrix4x4> previousMatrices = new Stack<Matrix4x4>();
+    public static Rect Begin(float zoomScale, Rect screenCoordsArea)
+    {
+        GUI.EndGroup();
+
+        Rect clippedArea = screenCoordsArea.ScaleSizeBy(1.0f / zoomScale, screenCoordsArea.min);
+        //clippedArea.y += 21f;
+
+        //Handles.DrawSolidRectangleWithOutline(clippedArea, Color.clear, Color.yellow);
+
+        GUI.BeginGroup(new Rect(0f, 21f / zoomScale, clippedArea.width + clippedArea.x, clippedArea.height + clippedArea.y));
+
+        previousMatrices.Push(GUI.matrix);
+        Matrix4x4 translation = Matrix4x4.TRS(screenCoordsArea.min, Quaternion.identity, Vector3.one);
+        Matrix4x4 scale = Matrix4x4.Scale(new Vector3(zoomScale, zoomScale, 1.0f));
+        GUI.matrix = translation * scale * translation.inverse;
+
+        return clippedArea;
+    }
+
+    /// <summary>
+    /// Ends the zoom area
+    /// </summary>
+    public static void End()
+    {
+        GUI.matrix = previousMatrices.Pop();
+        GUI.EndGroup();
+        GUI.BeginGroup(new Rect(0, 21, Screen.width, Screen.height));
+    }
+}
---
+++ Statics/Extension Methods/RectExtensions.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public static class RectExtensions
+{
+    public static Rect ScaleSizeBy(this Rect rect, float scale) 
+    { 
+        return rect.ScaleSizeBy(scale, rect.center); 
+    }
+    
+    public static Rect ScaleSizeBy(this Rect rect, float scale, Vector2 pivotPoint)
+    {
+        Rect result = rect;
+
+        //"translate" the top left to something like an origin
+        result.x -= pivotPoint.x;
+        result.y -= pivotPoint.y;
+
+        //Scale the rect
+        result.xMin *= scale;
+        result.yMin *= scale;
+        result.xMax *= scale;
+        result.yMax *= scale;
+
+        //"translate" the top left back to its original position
+        result.x += pivotPoint.x;
+        result.y += pivotPoint.y;
+
+        return result;
+    }
+}
